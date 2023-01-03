e54a538340580f87c459c4eb1f096dedb2c18a6d
Lucas Lago <llago@atlassian.com>
2023-01-04 09:20:28 +1100

    Add format autocmd

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -53,12 +53,7 @@ lsp.set_preferences({
 lsp.on_attach(function(client, bufnr)
   local opts = { buffer = bufnr, remap = false }
 
-  -- if client.name == "eslint" then
-  --   vim.cmd.LspStop('eslint')
-  --   return
-  -- end
-
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+  -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
   vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
   vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
   vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
@@ -69,6 +64,8 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
   vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
   vim.keymap.set("n", "gr", require('telescope.builtin').lsp_references, opts)
+  vim.keymap.set("n", "gi", require('telescope.builtin').lsp_implementations, opts)
+  vim.keymap.set("n", "gd", require('telescope.builtin').lsp_definitions, opts)
 end)
 
 lsp.setup()
---
+++ nvim/lua/llago/autocmd.lua
@@ -0,0 +1,5 @@
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = { '*.tsx', '*.ts', '*.jsx', '*.js' },
+  command = 'silent! EslintFixAll',
+  group = vim.api.nvim_create_augroup('MyAutocmdsJavaScripFormatting', {}),
+})
