a2ae98a4803900a6bb942b8b1f2cea77acd81ce7
Lucas Lago <llago@atlassian.com>
2023-01-03 21:47:06 +1100

    Fix lsp shut down on new buffers

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -53,10 +53,10 @@ lsp.set_preferences({
 lsp.on_attach(function(client, bufnr)
   local opts = { buffer = bufnr, remap = false }
 
-  if client.name == "eslint" then
-    vim.cmd.LspStop('eslint')
-    return
-  end
+  -- if client.name == "eslint" then
+  --   vim.cmd.LspStop('eslint')
+  --   return
+  -- end
 
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
   vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
@@ -69,7 +69,6 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
   vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
   vim.keymap.set("n", "gr", require('telescope.builtin').lsp_references, opts)
-  -- nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
 end)
 
 lsp.setup()
--- nvim/lua/llago/set.lua
+++ nvim/lua/llago/set.lua
@@ -13,7 +13,6 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
