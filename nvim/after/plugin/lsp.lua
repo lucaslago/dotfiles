88deef205524c8af0cb22584e5818d8a893ba696
Lucas Lago <llago@atlassian.com>
2023-01-04 12:07:16 +1100

    Show errrs on hover

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -53,6 +53,21 @@ lsp.set_preferences({
 lsp.on_attach(function(client, bufnr)
   local opts = { buffer = bufnr, remap = false }
 
+  vim.api.nvim_create_autocmd("CursorHold", {
+    buffer = bufnr,
+    callback = function()
+      local cursorHoldOpts = {
+        focusable = false,
+        close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+        border = 'rounded',
+        source = 'always',
+        prefix = ' ',
+        scope = 'cursor',
+      }
+      vim.diagnostic.open_float(nil, cursorHoldOpts)
+    end
+  })
+
   -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
   vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
   vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
