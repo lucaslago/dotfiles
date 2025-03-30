534ec319840dfe351ee60ea368850e97f4673db9
Lucas Lago <llago@atlassian.com>
2025-03-31 09:33:52 +1100

    Fix removal of pomodoro in lualine

--- nvim/lua/llago/plugins/lualine.lua
+++ nvim/lua/llago/plugins/lualine.lua
@@ -2,12 +2,5 @@ return { {
   "nvim-lualine/lualine.nvim",
   cond = not vim.g.vscode, -- Only enable when not in VS Code
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require('lualine').setup({
-      theme = 'gruvbox_dark',
-      sections = {
-        lualine_c = { 'filename', require('pomodoro').statusline }
-      }
-    })
-  end
+  opts = { theme = 'gruvbox_dark' }
 } }
